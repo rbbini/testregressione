dc30b3220e87560478e1a257b39aa3182338fada
rbbini <95579733+rbbini@users.noreply.github.com>
2022-01-18 22:23:31 +0100

    rimosso controllo sulle date
    mannaggia a me che ho la mente contorta

--- projectLourdes/main.py
+++ projectLourdes/main.py
@@ -1,11 +1,9 @@
-from flask import Flask, flash, request, render_template, jsonify, abort
-from werkzeug.utils import secure_filename
+from flask import Flask, flash, request, render_template, jsonify
 import os
 import pandas as pd
 from utils import preprocessing, predictions_hip_6months
 
 app = Flask(__name__)
-
 
 path = os.getcwd()
 UPLOAD_FOLDER = os.path.join(path, 'static')
@@ -14,20 +12,16 @@ UPLOAD_FOLDER = os.path.join(path, 'static')
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 
-
-SECRET_KEY = 'b\xfeT\x93\x1b\xe5\x9b\xe9\x9c\x1c@\xf6\xef\xbe\x81b\xc2\xd5(\xe9E\xab\xe8\xfe'
 ALLOWED_EXTENSIONS = {'xlsx', 'xls'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
-app.config['SECRET_KEY'] = SECRET_KEY
 
 
 # funzione per controllare che l'estensione del file sia accettabile
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,33 +31,37 @@ def home_page():
 
 @app.route('/data/analysis', methods=['POST'])
 def output():
+    # data_folder = os.path.join(path, '..', 'data')
+    # dataset = pd.read_excel(os.path.join(data_folder, 'data_reg_anca.xls'))
+    # data_preprocessed = preprocessing(dataset)
+
     if "dataSource" in request.form and request.form.get("dataSource") == "manually":
         file = request.files['file']
         if file.filename == '':
             flash("you have to select a file!")
-            return render_template('index.html')
+            return 'File inserito correttamente'
         if not allowed_file(file.filename):
             flash('only xlsx or xls files are accepted')
-            return render_template('index.html')
-        
+            return 'File inserito non valido'
+
         # SALVO DATASET NELLA CARTELLA static E LO LEGGO
-        #filename = secure_filename(file.filename)
-        #filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        #file.save(filepath)
-        
+        # why u do that??
+        # filename = secure_filename(file.filename)
+        # filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        # file.save(filepath)
+        # pd.read_excel(file)
         dataset = pd.read_excel(file)
         data_preprocessed = preprocessing(dataset)
-        predictions, other = predictions_hip_6months(data_preprocessed)
+        predictions = predictions_hip_6months(data_preprocessed)
         results = jsonify(predictions)
-        other_data = jsonify(other)
         
-        return results, other_data
+        return results
     
     elif "dataSource" in request.form and request.form.get("dataSource") == 'patientEpisode':
         form = request.form
         input_data = {
-            "Uid": form['uid'],
-            "Sesso": form['sesso'],
+            "Uid": form['uid'],  # .id_paziente.data
+            "Sesso": form['sesso'],  # .sesso.data
             "Anni ricovero": form['anni_ricovero'],
             "Data operazione": form['data_operazione'],
             "Data dimissione": form['data_dimissione'],
@@ -78,19 +76,17 @@ def output():
             "HOOSPS Total PreOp": form['anni_ricovero'],
             "BMI altezza risp PreOp": form['anni_ricovero'],
             "BMI peso risp PreOp": form['anni_ricovero'],
-            "BMI Total PreOp": form['anni_ricovero']
-            }
-        
-        input_data = pd.DataFrame.from_dict(input_data, orient = 'index').T
+            "BMI Total PreOp": form['anni_ricovero'],
+        }
+        input_data = pd.DataFrame.from_dict(input_data, orient='index').T
         data_preprocessed = preprocessing(input_data)
-        predictions, other = predictions_hip_6months(data_preprocessed)
+        predictions = predictions_hip_6months(data_preprocessed)
         results = jsonify(predictions)
-        other_data = jsonify(other)
-        return results, other_data
-    else:    
-        abort(400)
+        
+        return results
+    else:
+        return flash('Dati non validi')
 
 
-
-if __name__ =="__main__":  
-    app.run(debug = True)  
+if __name__ == "__main__":
+    app.run(debug=True)
--- projectLourdes/utils.py
+++ projectLourdes/utils.py
@@ -1,9 +1,10 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import pickle
 import statistics
 import os
 
+# data = pd.read_excel(r'C:\Users\Loredana\Documents\epimetheus-project\testregressione\data\data_reg_anca.xls')
 
 
 # dati che saranno ritornati in un json
@@ -14,35 +15,33 @@ ageDB = db['Anni ricovero'].to_numpy()
 ageDB = ageDB.astype(int)
 ageDB = ageDB.tolist()
 
-DB_6months_p = db['SF12 MentalScore 6months'].tolist() 
-DB_6months_m = db['SF12 PhysicalScore 6months'].tolist() 
+DB_6months_p = db['SF12 MentalScore 6months'].tolist()
+DB_6months_m = db['SF12 PhysicalScore 6months'].tolist()
 
 medianP = statistics.median(DB_6months_p)
 medianM = statistics.median(DB_6months_m)
 
-
 columns_preop = ['Uid', 'Sesso', 'Anni ricovero', 'Data operazione', 'Data dimissione',
-           'Nome evento', 'Nome equipe', 'Procedura intervento', 'HHS Function PreOp',	
-           'HHS Total PreOp', 'VAS PAIN risp PreOp','SF12 PhysicalScore PreOp',	
-           'SF12 MentalScore PreOp', 'HOOSPS Total PreOp', 'BMI altezza risp PreOp',
-           'BMI peso risp PreOp','BMI Total PreOp']
-
+                 'Nome evento', 'Nome equipe', 'Procedura intervento', 'HHS Function PreOp',
+                 'HHS Total PreOp', 'VAS PAIN risp PreOp', 'SF12 PhysicalScore PreOp',
+                 'SF12 MentalScore PreOp', 'HOOSPS Total PreOp', 'BMI altezza risp PreOp',
+                 'BMI peso risp PreOp', 'BMI Total PreOp']
 
 
 def preproces_nomeevento(data_preop):
-    data_preop['Nome evento'] = data_preop['Nome evento'].str.lower()#replace('\s+', ' ').value_counts()
-    data_preop['Nome evento'] = data_preop['Nome evento'].str.replace('\n', '', regex = True)
-    data_preop['Nome evento'] = data_preop['Nome evento'].str.replace(' +', ' ', regex = True)
-    data_preop['Nome evento'] = data_preop['Nome evento'].str.replace('([a-z]+)(protesi)', r'\1 protesi',regex = True)
+    data_preop['Nome evento'] = data_preop['Nome evento'].str.lower()  # replace('\s+', ' ').value_counts()
+    data_preop['Nome evento'] = data_preop['Nome evento'].str.replace('\n', '', regex=True)
+    data_preop['Nome evento'] = data_preop['Nome evento'].str.replace(' +', ' ', regex=True)
+    data_preop['Nome evento'] = data_preop['Nome evento'].str.replace('([a-z]+)(protesi)', r'\1 protesi', regex=True)
     return data_preop['Nome evento']
 
 
 def from_obj_to_num(x):
     try:
         res = x.astype(float)
-    except ValueError: 
-        res = x 
-    return res 
+    except ValueError:
+        res = x
+    return res
 
 
 def check_gender(x):
@@ -56,151 +55,168 @@ def check_gender(x):
 
 
 def check_equipe(df):
-    if not '36h orto - moroni' in df.columns:
+    if '36h orto - moroni' not in df.columns:
         df['36h orto - moroni'] = 0
-    if not '36p orto - parente' in df.columns:
+    if '36p orto - parente' not in df.columns:
         df['36p orto - parente'] = 0
-    if not 'casco' in df.columns:
-        df['casco'] = 0   
-    if not 'centro di traumatologia dello sport' in df.columns:
+    if 'casco' not in df.columns:
+        df['casco'] = 0
+    if 'centro di traumatologia dello sport' not in df.columns:
         df['centro di traumatologia dello sport'] = 0
-    if not 'chirurgia anca i' in df.columns:
+    if 'chirurgia anca i' not in df.columns:
         df['chirurgia anca i'] = 0
-    if not 'clinica ortopedica' in df.columns:
+    if 'clinica ortopedica' not in df.columns:
         df['clinica ortopedica'] = 0
-    if not 'e.u.o.r.r.' in df.columns:
+    if 'e.u.o.r.r.' not in df.columns:
         df['e.u.o.r.r.'] = 0
-    if not 'gspine4' in df.columns:
+    if 'gspine4' not in df.columns:
         df['gspine4'] = 0
-    if not 'oraco' in df.columns:
+    if 'oraco' not in df.columns:
         df['oraco'] = 0
-    if not 'ot9 orto - ventura2' in df.columns:
+    if 'ot9 orto - ventura2' not in df.columns:
         df['ot9 orto - ventura2'] = 0
     return df
 
 
 def check_event(df):
-    if not 'bilaterale protesi primo intervento' in df.columns:
+    if 'bilaterale protesi primo intervento' not in df.columns:
         df['bilaterale protesi primo intervento'] = 0
-    if not 'destra protesi primo intervento' in df.columns:
+    if 'destra protesi primo intervento' not in df.columns:
         df['destra protesi primo intervento'] = 0
-    if not 'destra revisione' in df.columns:
+    if 'destra revisione' not in df.columns:
         df['destra revisione'] = 0
-    if not 'sinistra protesi primo intervento' in df.columns:
+    if 'sinistra protesi primo intervento' not in df.columns:
         df['sinistra protesi primo intervento'] = 0
-    if not 'sinistra revisione' in df.columns:
+    if 'sinistra revisione' not in df.columns:
         df['sinistra revisione'] = 0
     return df
 
 
 def preprocessing(data):
     data = data.replace('#null', np.nan)
-    
+
     data = data.apply(from_obj_to_num)
 
-    #data = data[~data['SF12 MentalScore 6months'].isna()]
+    # data = data[~data['SF12 MentalScore 6months'].isna()]
 
-    #data = data[~data['SF12 PhysicalScore 6months'].isna()]
-    
+    # data = data[~data['SF12 PhysicalScore 6months'].isna()]
+
     data = data[~data['SF12 MentalScore PreOp'].isna()]
-    
+
     data = data[~data['SF12 PhysicalScore PreOp'].isna()]
-    
+
     data = data[~data['HOOSPS Total PreOp'].isna()]
-    
+
     data = data[~data['BMI altezza risp PreOp'].isna()]
-    
+
     data = data[~data['BMI peso risp PreOp'].isna()]
-    
-    data['Data operazione'] = data['Data operazione'].str.replace('\s(00:00:00.0)','', regex = True)
-    
+
+    data['Data operazione'] = data['Data operazione'].str.replace('\s(00:00:00.0)', '', regex=True)
+
     data['Data operazione'] = pd.to_datetime(data['Data operazione'])
-    
+
     data['Data operazione'] = data['Data operazione'].dt.strftime('%Y/%m/%d')
+
+    #mask_date = data['Data operazione'] <= '2021/05/05'
+
+    #data_clean = data.loc[mask_date, :]
+
+    #data_preop = data_clean.loc[:, columns_preop]\
     
-    mask_date = data['Data operazione'] <= '2021/05/05'
-    
-    data_clean = data.loc[mask_date,:]
-    
-    data_preop = data_clean.loc[:, columns_preop]
+    data_preop = data.loc[:, columns_preop]
 
     with open('lista_nome_evento.pkl', 'rb') as f:
         lista_nome_evento = pickle.load(f)
-        
+
     def normalize_nomevento(x):
         if x in lista_nome_evento:
             nome_evento = x
         else:
             nome_evento = np.nan
         return nome_evento
-    
+
     data_preop['Nome evento'] = preproces_nomeevento(data_preop)
 
     data_preop['Nome evento'] = data_preop['Nome evento'].apply(normalize_nomevento)
-    
+
     data_preop['Procedura intervento'] = data_preop['Procedura intervento'].str.lower()
-    
+
     data_preop['Procedura intervento'] = data_preop['Procedura intervento'].str.replace('sx', 'sinistra')
     data_preop['Procedura intervento'] = data_preop['Procedura intervento'].str.replace('dx', 'destra')
-    
-    data_preop['Nome equipe'] = data_preop['Nome equipe'].str.lower().str.replace(' +', ' ',regex = True)
-    
-    data_preop = data_preop[data_preop['Nome equipe'] != 'chirurgia del ginocchio i']
-    
-    data_preop = pd.concat([data_preop.drop(['Nome evento'], axis = 1), pd.get_dummies(data_preop['Nome evento'])], axis = 1)
 
-    data_preop = pd.concat([data_preop.drop(['Nome equipe'], axis = 1), pd.get_dummies(data_preop['Nome equipe'])], axis = 1)
-    
+    data_preop['Nome equipe'] = data_preop['Nome equipe'].str.lower().str.replace(' +', ' ', regex=True)
+
+    data_preop = data_preop[data_preop['Nome equipe'] != 'chirurgia del ginocchio i']
+
+    data_preop = pd.concat([data_preop.drop(['Nome evento'], axis=1), pd.get_dummies(data_preop['Nome evento'])],
+                           axis=1)
+
+    data_preop = pd.concat([data_preop.drop(['Nome equipe'], axis=1), pd.get_dummies(data_preop['Nome equipe'])],
+                           axis=1)
+
+    # data_preop.Sesso = pd.get_dummies(data_preop.Sesso, drop_first= True) #0 F 1 M
     data_preop['Sesso'] = data_preop['Sesso'].apply(check_gender)
     data_preop = check_equipe(data_preop)
     data_preop = check_event(data_preop)
-    
-    columns_to_drop = ['Data operazione', 'Data dimissione', 'Procedura intervento', 'HHS Function PreOp', 'HHS Total PreOp']    
-    
-    data_preop.drop(columns_to_drop, axis  = 1, inplace = True)
-    
+
+    columns_to_drop = ['Data operazione', 'Data dimissione', 'Procedura intervento', 'HHS Function PreOp',
+                       'HHS Total PreOp']
+
+    data_preop.drop(columns_to_drop, axis=1, inplace=True)
+
+    # dopo la colonna "Uid" (la prima) elimina tutte le colonne che non hanno nel nome "3months"/"6months"/"12months"
+    # data_3month = pd.concat([data_clean[['Uid']], data_clean.filter(regex='3months', axis=1)], axis = 1)
+
+    # data_6month = pd.concat([data_clean[['Uid']], data_clean.filter(regex='6months', axis=1)], axis = 1)
+
+    # data_12month = pd.concat([data_clean[['Uid']], data_clean.filter(regex='12months', axis=1)], axis = 1)
+
     return data_preop
 
 
 def predictions_hip_6months(data_to_pred):
     # drop dell'id perchè non riesce a convertirlo in float
-    data_to_pred.drop('Uid', axis = 1, inplace = True)
-    
+    data_to_pred.drop('Uid', axis=1, inplace=True)
+
     with open("model_6months_phy.pkl", 'rb') as file:
         loaded_model = pickle.load(file)
-    predictionsP = loaded_model.predict(data_to_pred).tolist() 
+    predictionsP = loaded_model.predict(data_to_pred).tolist()
     with open("model_6months_men.pkl", 'rb') as file:
         loaded_model2 = pickle.load(file)
-    predictionsM = loaded_model2.predict(data_to_pred).tolist() 
-    
+    predictionsM = loaded_model2.predict(data_to_pred).tolist()
+
     age = data_to_pred['Anni ricovero'].to_numpy()
     age = age.astype(int)
-    age = age.tolist()       
+    age = age.tolist()
     
-    # stime fatte sui dati in input 
-    estimation = {"SF12_PhysicalScore_6months": predictionsP, # score fisico dopo 6 mesi
-                  "SF12_MentalScore_6months": predictionsM, # score mentale dopo 6 mesi
-                  "age": age # eta' 
-    }
+    # stime fatte sui dati in input
+    estimation = {"SF12_PhysicalScore_6months": predictionsP,  # score fisico dopo 6 mesi
+                  "SF12_MentalScore_6months": predictionsM,  # score mentale dopo 6 mesi
+                  "age": age  # eta'
+                  }
     
     # dati dei pazienti nel nostro db (data_reg_anca.xls)
-    DBstuff = {"SF12_PhysicalScore_6months_DB": DB_6months_p, # score fisico dopo 6 mesi
-                  "SF12_MentalScore_6months_DB": DB_6months_m, # score mentale dopo 6 mesi
-                  "ageDB": ageDB, # eta'
-                  "medianaP": medianP, # mediana degli score fisici
-                  "medianaM": medianM # mediana degli score mentali
-    }      
+    DBstuff = {"SF12_PhysicalScore_6months_DB": DB_6months_p,  # score fisico dopo 6 mesi
+               "SF12_MentalScore_6months_DB": DB_6months_m,  # score mentale dopo 6 mesi
+               "ageDB": ageDB,  # eta'
+               "medianaP": medianP,  # mediana degli score fisici
+               "medianaM": medianM  # mediana degli score mentali
+               }
 
-    return estimation, DBstuff
+    to_json = {
+        "estimation" : estimation,
+        "DBstuff" : DBstuff
+        }
+
+    return to_json
 
 
-
-
+# data_prepr = preprocessing(data)
+# predictions_hip_6months(data_prepr)
 
 
 # -------------------- PER TESTING --------------------------------------------
 """
-data = pd.read_excel(r'E:\Stage\epimetheus\app\data')
 data_prepr = preprocessing(data)
 e = predictions_hip_6months(data_prepr)
 print(e)
@@ -225,10 +241,6 @@ input_data = {
     }
 input_data = pd.DataFrame.from_dict(input_data, orient='index').T
 data_preprocessed = preprocessing(input_data)
-estimation, estim2 = predictions_hip_6months(data_preprocessed)
-#print(estimation)
+estimation = predictions_hip_6months(data_preprocessed)
+print(estimation)
 """
-
-
-
-    
